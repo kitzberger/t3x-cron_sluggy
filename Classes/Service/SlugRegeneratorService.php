e131bdc79a4330d449e2b566833c9b123cb32200
Ernesto Baschny <eb@cron.eu>
2021-03-09 01:28:01 +0100

    Support b13/masi exclude_slug_for_subpages when regenerating slugs through CLI

--- Classes/Service/SlugRegeneratorService.php
+++ Classes/Service/SlugRegeneratorService.php
@@ -157,9 +157,15 @@ class SlugRegeneratorService implements SiteAwareInterface
 
         // Prefix the path to the parents from our cache if required
         if (!$useParentPrefix && $this->slugCache[$row['pid']]) {
+            $cachedParent = $this->slugCache[$row['pid']];
             $slug = $this->slugCache[$row['pid']] . $slug;
         }
-        $this->slugCache[$row['uid']] = $slug === '/' ? '' : $slug;
+        // support b13/masi exclusions
+        if ((bool)$row['exclude_slug_for_subpages']) {
+            $this->slugCache[$row['uid']] = $cachedParent;
+        } else {
+            $this->slugCache[$row['uid']] = $slug === '/' ? '' : $slug;
+        }
 
         // Make sure it is unique
         $state = RecordStateFactory::forName('pages')
